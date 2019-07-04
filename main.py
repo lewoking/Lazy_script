a020c1ab85ffbb69d7baf2f2deb7f4628c2d1c82
guozi2 <lewoking@gmail.com>
2019-07-04 16:21:59 +0800

    int rel

--- main.py
+++ main.py
@@ -1,58 +1,60 @@
 import pyautogui as pg
+import time
 
 pg.FAILSAFE = True
 # pg.PAUSE = 0.5
 
 width, height = pg.size()
 
-x, y = pg.position()
+x, y = pg.position()  # 手动定位点
 
 # or 以图定位
 # p = pg.locateOnScreen('find.png')
 # x,y= pg.center(find)
 
 f = pg.locateOnScreen('yes.png')
-px, py = pg.center(f)
+px, py = pg.center(f)  # 自动定位点
 
 pg.click(x, y)
 
-pg.click(px-202, py-202)
+pg.click(px, py + 60)
 
-pg.click(px, py)
+pg.click(px, py + 230)
 
 try:
-    pg.locateOnScreen('sucess.png')
+    time.sleep(1)  # 等待提示框弹出时间
+#  pg.locateOnScreen('sucess.png')  # 自动识别成功提示框
 except expression as identifier:
 
     pass
 else:
     pg.typewrite(['enter'])
 
-pg.click(x, y + 18)
-pg.click(x, y + 18)
+pg.click(x, y + 30)
+pg.click(x, y + 30)
 
+pg.click(px, py + 100)
 
-pg.click(px, py -155)
-
-pg.click(px, py)
+pg.click(px, py + 230)
 
 try:
-    pg.locateOnScreen('sucess.png')
+    time.sleep(1)
+#    pg.locateOnScreen('sucess.png')
 except expression as identifier:
     pass
 else:
     pg.typewrite(['enter'])
 
-pg.click(x, y + 47)
-pg.click(x, y + 47)
+pg.click(x, y + 60)
+pg.click(x, y + 60)
 
+pg.click(px, py + 140)
 
-pg.click(px, py-117)
-
-pg.click(px, py)
+pg.click(px, py + 230)
 
 try:
-    pg.locateOnScreen('sucess.png')
+    time.sleep(1)
+#    pg.locateOnScreen('sucess.png')
 except expression as identifier:
     pass
 else:
