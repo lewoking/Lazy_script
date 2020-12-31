455ddde89cd067a47d62ec82d9d581cf941aba8a
lewoking <lewoking@gmail.com>
2020-12-31 17:59:53 +0800

    src

--- maxPower.py
+++ maxPower.py
@@ -1,23 +1,46 @@
 import pandas as pd  
-
 from sqlalchemy import create_engine  
-
 import cx_Oracle  
-
 db=cx_Oracle.connect('userid','password','10.10.1.10:1521/dbinstance')  
-
 print (db.version)  
-
 cr=db.cursor()  
-
 sql='select * from sys_user'  
-
 cr.execute(sql)  
-
 rs=cr.fetchall()  
-
 zz=pd.DataFrame(rs);  
-
 print (zz)  
-
 db.close() 
+
+#coding=utf-8
+from pandas import Series,DataFrame,merge
+import numpy as np
+data=DataFrame([{"id":0,"name":'lxh',"age":20,"cp":'lm'},{"id":1,"name":'xiao',"age":40,"cp":'ly'},{"id":2,"name":'hua',"age":4,"cp":'yry'},{"id":3,"name":'be',"age":70,"cp":'old'}])
+data1=DataFrame([{"id":100,"name":'lxh','cs':10},{"id":101,"name":'xiao','cs':40},{"id":102,"name":'hua2','cs':50}])
+data2=DataFrame([{"id":0,"name":'lxh','cs':10},{"id":101,"name":'xiao','cs':40},{"id":102,"name":'hua2','cs':50}])
+ 
+print "单个列名做为内链接的连接键\r\n",merge(data,data1,on="name",suffixes=('_a','_b'))
+print "多列名做为内链接的连接键\r\n",merge(data,data2,on=("name","id"))
+print '不指定on则以两个DataFrame的列名交集做为连接键\r\n',merge(data,data2) #这里使用了id与name
+ 
+#使用右边的DataFrame的行索引做为连接键
+##设置行索引名称
+indexed_data1=data1.set_index("name")
+print "使用右边的DataFrame的行索引做为连接键\r\n",merge(data,indexed_data1,left_on='name',right_index=True)
+ 
+ 
+print '左外连接\r\n',merge(data,data1,on="name",how="left",suffixes=('_a','_b'))
+print '左外连接1\r\n',merge(data1,data,on="name",how="left")
+print '右外连接\r\n',merge(data,data1,on="name",how="right")
+data3=DataFrame([{"mid":0,"mname":'lxh','cs':10},{"mid":101,"mname":'xiao','cs':40},{"mid":102,"mname":'hua2','cs':50}])
+ 
+#当左右两个DataFrame的列名不同，当又想做为连接键时可以使用left_on与right_on来指定连接键
+print "使用left_on与right_on来指定列名字不同的连接键\r\n",merge(data,data3,left_on=["name","id"],right_on=["mname","mid"])
+
+
+# union 影响效率？
+
+# 使用  select max(cur_67),time from yc_his_0001  where    最大发生时间不一致 不能联合。
+sql = select max(%s),time from %s  where to_char(time,'yyyy')=to_char(sysdate,'yyyy')  #本年度
+
+
+  select * from 表 where time>=TRUNC(SYSDATE, 'MM') and time<=last_day(SYSDATE) #查询本月
